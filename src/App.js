feec7f7b929c4f93b752ec7e37b6a8d2e26122f4
apsbundela <bundela.ayush99@gmail.com>
2022-01-05 16:31:24 +0530

    b1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <Headertag></Headertag>
+        <Mytag></Mytag>
+        <Mytag></Mytag>
+        <Mytag></Mytag>
+        <Mytag></Mytag>
+        <Mytag></Mytag>
+        <Mytag></Mytag>
+        <Mytag></Mytag>
+        <Mytag></Mytag>
+      </div>
     </div>
   );
 }
 
-export default App;
+function Mytag() {
+  return (
+    <div>
+      <div className="bg-info">
+        <h3 className="mt-2">I am Creating My First Tag</h3>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa
+          voluptatibus, architecto maiores consequuntur cumque, iste eos sed,
+          fugiat eius recusandae et itaque quae explicabo obcaecati odit quasi
+          placeat adipisci alias!
+        </p>
+      </div>
+    </div>
+  );
+}
+
+function Headertag() {
+  return (
+    <div className="container-fluid sticky-top">
+      <div className="row bg-dark text-light fs-4 p-3">React</div>
+    </div>
+  );
+}
